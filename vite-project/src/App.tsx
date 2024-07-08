ff2611171d6984f697f6c98782574c51e3a7a834
Jessy Yeh <jiejun.yeh@gmail.com>
2024-07-08 19:16:00 +0100

    Create Navbar for mobile view

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,7 +1,16 @@
 import "./App.css";
+import Box from "@mui/material/Box";
+import Navbar from "./components/Navbar/Navbar";
+import { useState } from "react";
 
 function App() {
-  return <></>;
+  const [hamburgerMenuOpen, setHamburgerMenuOpen] = useState(false);
+
+  return (
+    <Box>
+      <Navbar open={hamburgerMenuOpen} setOpen={setHamburgerMenuOpen} />
+    </Box>
+  );
 }
 
 export default App;
