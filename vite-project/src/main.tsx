a6dd3efe3c7e3465e05c90f578b6a4a972378b53
Jessy Yeh <jiejun.yeh@gmail.com>
2024-07-08 18:28:43 +0100

    install MUI

--- vite-project/src/main.tsx
+++ vite-project/src/main.tsx
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import CssBaseline from "@mui/material/CssBaseline";
+import { StyledEngineProvider } from "@mui/material/styles";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <CssBaseline />
-    <App />
+    <StyledEngineProvider injectFirst>
+      <App />
+    </StyledEngineProvider>
   </React.StrictMode>
 );
