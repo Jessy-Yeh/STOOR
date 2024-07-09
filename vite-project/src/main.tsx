37491e5c14aec80998746726b01a71072340f83c
Jessy Yeh <jiejun.yeh@gmail.com>
2024-07-09 13:26:48 +0100

    use ThemeProvider instead

--- vite-project/src/main.tsx
+++ vite-project/src/main.tsx
@@ -2,13 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import CssBaseline from "@mui/material/CssBaseline";
-import { StyledEngineProvider } from "@mui/material/styles";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+
+const theme = createTheme();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <CssBaseline />
-    <StyledEngineProvider injectFirst>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
       <App />
-    </StyledEngineProvider>
+    </ThemeProvider>
   </React.StrictMode>
 );
