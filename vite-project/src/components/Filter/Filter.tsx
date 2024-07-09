1cb57819dee8001f6bb87d760b6c593528eacad0
Jessy Yeh <jiejun.yeh@gmail.com>
2024-07-09 22:44:51 +0100

    implement filter function and display filtered products

--- vite-project/src/components/Filter/Filter.tsx
+++ vite-project/src/components/Filter/Filter.tsx
@@ -1,123 +1,89 @@
-// import { Theme, useTheme } from "@mui/material/styles";
-// import Box from "@mui/material/Box";
-// import OutlinedInput from "@mui/material/OutlinedInput";
-// import InputLabel from "@mui/material/InputLabel";
-// import MenuItem from "@mui/material/MenuItem";
-// import FormControl from "@mui/material/FormControl";
-// import Select, { SelectChangeEvent } from "@mui/material/Select";
-// import Chip from "@mui/material/Chip";
-import { useState } from "react";
-import { Checkbox, FormGroup, Typography } from "@mui/material";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import { Checkbox, Typography } from "@mui/material";
 import FormControlLabel from "@mui/material/FormControlLabel";
 
-// const ITEM_HEIGHT = 48;
-// const ITEM_PADDING_TOP = 8;
-// const MenuProps = {
-//   PaperProps: {
-//     style: {
-//       maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
-//       width: 250,
-//     },
-//   },
-// };
+interface FilterProps {
+  categories: string[];
+}
 
-// const categories = ["Men's Clothing", "Jewellery", "Electronics"];
+const Filter = ({ categories }: FilterProps) => {
+  const [searchParams] = useSearchParams();
+  const activeFilters: Array<string> =
+    searchParams.get("categories")?.split(",") || [];
 
-// function getStyles(name: string, personName: readonly string[], theme: Theme) {
-//   return {
-//     fontWeight:
-//       personName.indexOf(name) === -1
-//         ? theme.typography.fontWeightRegular
-//         : theme.typography.fontWeightMedium,
-//   };
-// }
+  // we want the checkboxes to be checked or not checked based on this value
+  // this is step 1 :)
+  // step 2 we can do later, which is to add the onChange function to update the url when we click one of the boxes
 
-const Filter = () => {
-  const [checked, setChecked] = useState(false);
+  const navigate = useNavigate();
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setChecked(event.target.checked);
+    // we want to update the value of the "categories" url query parameter
+    // to do this, we need to know:
+    // 1) what is already checked (if any)
+    // 2) which category the user clicked
+
+    const { name } = event.target;
+    const isChecked = activeFilters.includes(name);
+
+    const newActiveFilters = [...activeFilters];
+
+    if (isChecked) {
+      const indexOfCategory = activeFilters.findIndex(
+        (category) => category === name
+      );
+
+      if (indexOfCategory > -1) {
+        newActiveFilters.splice(indexOfCategory, 1);
+      }
+    } else {
+      newActiveFilters.push(name);
+    }
+
+    const newCategoriesValue = newActiveFilters.join(",");
+
+    navigate({
+      pathname: "/",
+      search:
+        newActiveFilters.length === 0
+          ? ""
+          : `?categories=${newCategoriesValue}`,
+    });
   };
-
-  // const handleChange1 = (event: React.ChangeEvent<HTMLInputElement>) => {
-  //   setChecked([event.target.checked, event.target.checked]);
-  // };
-
-  // const handleChange2 = (event: React.ChangeEvent<HTMLInputElement>) => {
-  //   setChecked([event.target.checked, checked[1]]);
-  // };
-
-  // const handleChange3 = (event: React.ChangeEvent<HTMLInputElement>) => {
-  //   setChecked([checked[0], event.target.checked]);
-  // };
-
-  // const children = (
-  //   <Box sx={{ display: "flex", flexDirection: "column", ml: 3 }}>
-  //     <FormControlLabel
-  //       label="Child 1"
-  //       control={<Checkbox checked={checked[0]} onChange={handleChange2} />}
-  //     />
-  //     <FormControlLabel
-  //       label="Child 2"
-  //       control={<Checkbox checked={checked[1]} onChange={handleChange3} />}
-  //     />
-  //   </Box>
-  // );
-
-  // const theme = useTheme();
-
-  // const [category, setCategory] = useState<string[]>([]);
-
-  // const handleChange = (event: SelectChangeEvent<typeof category>) => {
-  //   const {
-  //     target: { value },
-  //   } = event;
-  //   setCategory(typeof value === "string" ? value.split(",") : value);
-  // };
 
   return (
     <div>
-      {/* <FormControl sx={{ m: 1, width: 300 }}>
-        <InputLabel id="demo-multiple-chip-label">Filter</InputLabel>
-        <Select
-          labelId="demo-multiple-chip-label"
-          id="demo-multiple-chip"
-          multiple
-          value={category}
-          onChange={handleChange}
-          input={<OutlinedInput id="select-multiple-chip" label="Chip" />}
-          renderValue={(selected) => (
-            <Box sx={{ display: "flex", flexWrap: "wrap", gap: 0.5 }}>
-              {selected.map((value) => (
-                <Chip key={value} label={value} />
-              ))}
-            </Box>
-          )}
-          MenuProps={MenuProps}
-        >
-          {categories.map((name) => (
-            <MenuItem
-              key={name}
-              value={name}
-              style={getStyles(name, category, theme)}
-            >
-              {name}
-            </MenuItem>
-          ))}
-        </Select>
-      </FormControl> */}
-
       <div>
         <Typography>Filter by category</Typography>
-        <FormControlLabel
-          control={<Checkbox />}
-          label="Men's Clothing"
-          onChange={handleChange}
-        />
-        <FormControlLabel control={<Checkbox />} label="Jewellery" />
-        <FormControlLabel control={<Checkbox />} label="Electronics" />
+        {categories.map((category) => {
+          return (
+            <FilterOption
+              key={category}
+              category={category}
+              checked={activeFilters.includes(category)}
+              onChange={handleChange}
+            />
+          );
+        })}
       </div>
     </div>
+  );
+};
+
+interface FilterOptionProps {
+  checked: boolean;
+  category: string;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+const FilterOption = ({ checked, category, onChange }: FilterOptionProps) => {
+  return (
+    <FormControlLabel
+      control={
+        <Checkbox checked={checked} onChange={onChange} name={category} />
+      }
+      label={category}
+    />
   );
 };
 
--- vite-project/src/components/Home/Home.tsx
+++ vite-project/src/components/Home/Home.tsx
@@ -8,6 +8,8 @@ import Paper from "@mui/material/Paper";
 import Navbar from "../Navbar/Navbar";
 import Filter from "../Filter/Filter";
 import Banner from "../Banner/Banner";
+import { useSearchParams } from "react-router-dom";
+import { ProductType } from "../../types";
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
@@ -18,33 +20,48 @@ const Item = styled(Paper)(({ theme }) => ({
 }));
 
 const Home = () => {
-  const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState<ProductType[]>([]);
+  const [searchParams] = useSearchParams();
+  const activeFilters: Array<string> =
+    searchParams.get("categories")?.split(",") || [];
 
   useEffect(() => {
     axios.get("https://fakestoreapi.com/products").then((response) => {
-      console.log(response.data);
       setProducts(response.data);
     });
   }, []);
+
+  const categories = products.map((product) => product.category);
+  const uniqueCategories = [...new Set(categories)];
+
+  const filteredProducts = products.filter((product) => {
+    if (activeFilters.length === 0) {
+      return true;
+    }
+    return activeFilters.includes(product.category);
+  });
+
   return (
     <>
       <Navbar />
-      <Filter />
-      <Banner />
-      <Grid
-        container
-        spacing={2}
-        columns={{ xs: 4, sm: 8, md: 12 }}
-        rowSpacing={2}
-      >
-        {products.map((product, index) => (
-          <Grid item xs={6} md={4} key={index}>
-            <Item>
-              <Product product={product} />
-            </Item>
-          </Grid>
-        ))}
-      </Grid>
+      <div style={{ marginTop: "110px" }}>
+        <Filter categories={uniqueCategories} />
+        <Banner />
+        <Grid
+          container
+          spacing={2}
+          columns={{ xs: 4, sm: 8, md: 12 }}
+          rowSpacing={2}
+        >
+          {filteredProducts.map((product, index) => (
+            <Grid item xs={6} md={4} key={index}>
+              <Item>
+                <Product product={product} />
+              </Item>
+            </Grid>
+          ))}
+        </Grid>
+      </div>
     </>
   );
 };
