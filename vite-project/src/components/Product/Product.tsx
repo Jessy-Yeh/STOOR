9f6123664f234cb3fbbdb18187c2f1f9eec049b2
Jessy Yeh <jiejun.yeh@gmail.com>
2024-07-09 13:25:01 +0100

    rename components

--- vite-project/src/components/Product/Product.tsx
+++ vite-project/src/components/Product/Product.tsx
@@ -1,56 +1,111 @@
-import { Typography } from "@mui/material";
-import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
 import Stack from "@mui/material/Stack";
+import Navbar from "../Navbar/Navbar";
+import {
+  Button,
+  Collapse,
+  Divider,
+  List,
+  ListItemButton,
+  ListItemText,
+  Typography,
+} from "@mui/material";
+import Box from "@mui/material/Box";
+import InputLabel from "@mui/material/InputLabel";
+import FormControl from "@mui/material/FormControl";
+import NativeSelect from "@mui/material/NativeSelect";
+import { useState } from "react";
+import { ExpandLess, ExpandMore } from "@mui/icons-material";
 
-type Product = {
-  id: number;
-  title: string;
-  price: number;
-  description: string;
-  category: string;
-  image: string;
-  rating: {
-    rate: number;
-    count: number;
-  };
-};
+const Product = () => {
+  const [descriptionOpen, setDescriptionOpen] = useState(true);
+  const [ratingOpen, setRatingOpen] = useState(true);
+  const [deliveryOpen, setDeliveryOpen] = useState(true);
 
-interface Props {
-  product: Product;
-}
-
-const Product = ({ product }: Props) => {
   return (
-    <Box>
-      <img
-        src={product.image}
-        alt={product.description}
-        style={{ width: "100%" }}
-      />
-      <Stack
-        direction="row"
-        justifyContent="space-between"
-        sx={{
-          textOverflow: "ellipsis",
-          whiteSpace: "nowrap",
-          gap: "2rem",
-          maxWidth: "100%",
-        }}
-      >
-        <Typography>{product.title}</Typography>
-        <Typography>£{product.price}</Typography>
+    <Box p={1}>
+      <Navbar />
+      <Stack sx={{ marginTop: "6rem" }}>
+        <img />
+        <Stack>
+          <Typography variant="h2" sx={{ fontSize: "32px" }}>
+            Product Name
+          </Typography>
+          <Box sx={{ marginTop: "1rem" }}>
+            <FormControl fullWidth>
+              <InputLabel variant="standard" htmlFor="uncontrolled-native">
+                SELECT SIZE
+              </InputLabel>
+              <NativeSelect
+                defaultValue={30}
+                inputProps={{
+                  name: "SELECT SIZE",
+                  id: "uncontrolled-native",
+                }}
+              >
+                <option value={"small"}>SMALL</option>
+                <option value={"medium"}>MEDIUM</option>
+                <option value={"large"}>LARGE</option>
+              </NativeSelect>
+            </FormControl>
+          </Box>
+          <Button
+            variant="contained"
+            sx={{
+              backgroundColor: "black",
+              marginTop: "1rem",
+              width: "100%",
+            }}
+          >
+            ADD TO BAG
+          </Button>
+
+          <List
+            sx={{
+              width: "100%",
+              bgcolor: "background.paper",
+              marginTop: "2rem",
+            }}
+            component="nav"
+            aria-labelledby="nested-list-subheader"
+          >
+            <ListItemButton
+              onClick={() => setDescriptionOpen(!descriptionOpen)}
+            >
+              <ListItemText primary="DESCRIPTION" />
+              {descriptionOpen ? <ExpandLess /> : <ExpandMore />}
+            </ListItemButton>
+            <Collapse in={descriptionOpen} timeout="auto" unmountOnExit>
+              <Typography>product's description</Typography>
+            </Collapse>
+            <Divider />
+
+            <ListItemButton onClick={() => setRatingOpen(!ratingOpen)}>
+              <ListItemText primary="PRODUCT RATING" />
+              {ratingOpen ? <ExpandLess /> : <ExpandMore />}
+            </ListItemButton>
+            <Collapse in={ratingOpen} timeout="auto" unmountOnExit>
+              <Typography>product's rating</Typography>
+            </Collapse>
+            <Divider />
+
+            <ListItemButton onClick={() => setDeliveryOpen(!deliveryOpen)}>
+              <ListItemText primary="DELIVERY OPTIONS" />
+              {deliveryOpen ? <ExpandLess /> : <ExpandMore />}
+            </ListItemButton>
+            <Collapse in={deliveryOpen} timeout="auto" unmountOnExit>
+              <List component="div" disablePadding>
+                <ListItemButton sx={{ pl: 4 }}>
+                  <ListItemText primary="Collect from the store" />
+                </ListItemButton>
+                <ListItemButton sx={{ pl: 4 }}>
+                  <ListItemText primary="delivery to home" />
+                </ListItemButton>
+              </List>
+            </Collapse>
+            <Divider />
+          </List>
+        </Stack>
       </Stack>
-      <Stack direction="row" spacing={2}>
-        <Typography>{product.rating.rate}</Typography>
-        <Typography>from {product.rating.count} reviews</Typography>
-      </Stack>
-      <Button
-        variant="contained"
-        sx={{ backgroundColor: "black", margin: "1rem", width: "100%" }}
-      >
-        VIEW PRODUCT
-      </Button>
     </Box>
   );
 };
---
+++ vite-project/src/components/ProductCard/ProductCard.tsx
@@ -0,0 +1,61 @@
+import { Typography } from "@mui/material";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
+import { NavLink } from "react-router-dom";
+
+type Product = {
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+  category: string;
+  image: string;
+  rating: {
+    rate: number;
+    count: number;
+  };
+};
+
+interface Props {
+  product: Product;
+}
+
+const ProductCard = ({ product }: Props) => {
+  return (
+    <Box>
+      <img
+        src={product.image}
+        alt={product.description}
+        style={{ width: "100%" }}
+      />
+      <Stack
+        direction="row"
+        justifyContent="space-between"
+        sx={{
+          textOverflow: "ellipsis",
+          whiteSpace: "nowrap",
+          gap: "2rem",
+          maxWidth: "100%",
+        }}
+      >
+        <Typography>{product.title}</Typography>
+        <Typography>£{product.price}</Typography>
+      </Stack>
+      <Stack direction="row" spacing={2}>
+        <Typography>{product.rating.rate}</Typography>
+        <Typography>from {product.rating.count} reviews</Typography>
+      </Stack>
+      <NavLink to="products/">
+        <Button
+          variant="contained"
+          sx={{ backgroundColor: "black", margin: "1rem", width: "100%" }}
+        >
+          VIEW PRODUCT
+        </Button>
+      </NavLink>
+    </Box>
+  );
+};
+
+export default ProductCard;
