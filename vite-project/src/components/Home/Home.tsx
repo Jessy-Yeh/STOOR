1dbb2acbd5bc55ec1883e2dcb247296d2e30cc1e
Jessy Yeh <jiejun.yeh@gmail.com>
2024-07-09 13:26:09 +0100

    add other components to Home

--- vite-project/src/components/Home/Home.tsx
+++ vite-project/src/components/Home/Home.tsx
@@ -1,10 +1,13 @@
 import axios from "axios";
-import Box from "@mui/material/Box";
+// import Box from "@mui/material/Box";
 import { useEffect, useState } from "react";
-import Product from "../Product/Product";
+import Product from "../ProductCard/ProductCard";
 import Grid from "@mui/material/Grid";
 import { styled } from "@mui/material/styles";
 import Paper from "@mui/material/Paper";
+import Navbar from "../Navbar/Navbar";
+import Filter from "../Filter/Filter";
+import Banner from "../Banner/Banner";
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
@@ -24,7 +27,10 @@ const Home = () => {
     });
   }, []);
   return (
-    <Box>
+    <>
+      <Navbar />
+      <Filter />
+      <Banner />
       <Grid
         container
         spacing={2}
@@ -39,7 +45,7 @@ const Home = () => {
           </Grid>
         ))}
       </Grid>
-    </Box>
+    </>
   );
 };
 
