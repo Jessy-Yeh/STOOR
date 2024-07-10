b2739f3a65a4fbf62aedebe1b4403f00411d8409
Jessy Yeh <jiejun.yeh@gmail.com>
2024-07-10 10:05:42 +0100

    convert rating to star icon

--- vite-project/src/components/ProductCard/ProductCard.tsx
+++ vite-project/src/components/ProductCard/ProductCard.tsx
@@ -1,5 +1,5 @@
-import { Typography } from "@mui/material";
-import Box from "@mui/material/Box";
+import { Box, Rating, Typography } from "@mui/material";
+import StarIcon from "@mui/icons-material/Star";
 import Button from "@mui/material/Button";
 import Stack from "@mui/material/Stack";
 import { NavLink } from "react-router-dom";
@@ -11,11 +11,11 @@ interface Props {
 
 const ProductCard = ({ product }: Props) => {
   return (
-    <Box>
+    <Stack sx={{ width: { xs: "400px", md: "300px" } }}>
       <img
         src={product.image}
         alt={product.description}
-        style={{ width: "100%" }}
+        style={{ width: "100%", height: "100%" }}
       />
       <Stack
         direction="row"
@@ -31,18 +31,36 @@ const ProductCard = ({ product }: Props) => {
         <Typography>£{product.price}</Typography>
       </Stack>
       <Stack direction="row" spacing={2}>
-        <Typography>{product.rating.rate}</Typography>
+        {/* <Typography>{product.rating.rate}</Typography> */}
+        <Box
+          sx={{
+            width: 200,
+            display: "flex",
+            alignItems: "center",
+          }}
+        >
+          <Rating
+            name="rating"
+            value={product.rating.rate}
+            readOnly
+            precision={0.5}
+            sx={{ color: "black" }}
+            emptyIcon={
+              <StarIcon style={{ opacity: 0.55 }} fontSize="inherit" />
+            }
+          />
+        </Box>
         <Typography>from {product.rating.count} reviews</Typography>
       </Stack>
       <NavLink to={`product/${product.id}`}>
         <Button
           variant="contained"
-          sx={{ backgroundColor: "black", margin: "1rem", width: "100%" }}
+          sx={{ backgroundColor: "black", marginTop: "1rem", width: "100%" }}
         >
           VIEW PRODUCT
         </Button>
       </NavLink>
-    </Box>
+    </Stack>
   );
 };
 
