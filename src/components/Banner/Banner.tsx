02e3c961c76280b79220177baf3215fe58e9338f
Jessy Yeh <jiejun.yeh@gmail.com>
2024-08-02 15:23:11 +0100

    chore: confirgure tailwind css

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -1,35 +1,24 @@
-import { Divider, Typography } from "@mui/material";
-import Stack from "@mui/material/Stack";
+import { Divider } from "@mui/material";
 
 const Banner = () => {
   return (
     <>
-      <Stack
-        justifyContent="center"
-        alignItems="center"
-        sx={{ padding: "2rem 0" }}
-      >
-        <Typography
-          variant="h2"
-          m="0 0 1rem 0"
-          sx={{ fontSize: "24px", fontWeight: "500" }}
-        >
-          Our Clothing
-        </Typography>
-        <Typography
-          sx={{
-            maxWidth: "700px",
-            textAlign: "center",
-            fontSize: "14px",
-          }}
-        >
-          Lorem ipsum dolor sit amet consectetur. Nullam leo condimentum turpis
-          aliquet. Fermentum purus amet vitae sed nam imperdiet. Sit cursus sed
-          commodo aliquet ultricies mi volutpat tortor at. A gravida enim ut
-          quis et in lectus. Dolor tortor facilisi egestas faucibus nulla. Nibh
-          accumsan felis tempor convallis nunc porta integer.
-        </Typography>
-      </Stack>
+      <div className=" px-6 py-10 sm:py-20 lg:px-8 ">
+        <div className="mx-auto max-w-2xl text-center ">
+          <h2 className="text-4xl font-bold tracking-tight sm:text-6xl">
+            Our Clothing
+          </h2>
+          <p className="mt-6 text-lg leading-8 text-gray-600">
+            Lorem ipsum dolor sit amet consectetur. Nullam leo condimentum
+            turpis aliquet. Fermentum purus amet vitae sed nam imperdiet. Sit
+            cursus sed commodo aliquet ultricies mi volutpat tortor at. A
+            gravida enim ut quis et in lectus. Dolor tortor facilisi egestas
+            faucibus nulla. Nibh accumsan felis tempor convallis nunc porta
+            integer.
+          </p>
+        </div>
+      </div>
+
       <Divider />
     </>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
+import "./index.css";
 
 const theme = createTheme({
   typography: {
